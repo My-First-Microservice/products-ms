51efb02064239c83ca5eea5dad47102500ca1594
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2024-12-28 02:45:02 -0500

    Prisma configured

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -3,10 +3,12 @@ import { IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
-  name: string;
+  public name: string;
 
-  @IsNumber()
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
   @Min(0)
   @Type(() => Number)
-  price: number;
+  public price: number;
 }
